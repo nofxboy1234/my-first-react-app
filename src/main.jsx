6cc19b0b3bcaf910903f158f287b0a627a252c55
Dylan Palmboom <nofxboy1234@gmail.com>
2024-07-16 14:11:44 +0200

    Create MultiElement React Component

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import Hello from './Greeting.jsx';
 import { MacaroniCheese, FriedEggs } from './Food.jsx';
+import MultiElement from './MultiElement.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -11,5 +12,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Hello />
     <MacaroniCheese />
     <FriedEggs />
+    <MultiElement />
   </React.StrictMode>
 );
