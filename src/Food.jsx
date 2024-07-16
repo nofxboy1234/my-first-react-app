8ca7fbf48a7a5ab66f7af19b3606f1217e188d0e
Dylan Palmboom <nofxboy1234@gmail.com>
2024-07-16 09:13:56 +0200

    Create another React component and import it and use inside main.jsx

---
+++ src/Food.jsx
@@ -0,0 +1,11 @@
+function MacaroniCheese() {
+  return (
+    <img
+      src="/src/assets/macaroni.jpg"
+      width="300px"
+      alt="Delicious Macaroni Cheese"
+    />
+  );
+}
+
+export default MacaroniCheese;
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import Hello from './Greeting.jsx';
+import MacaroniCheese from './Food.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Hello />
+    <MacaroniCheese />
   </React.StrictMode>
 );
