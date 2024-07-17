b5d4c9abed2b813dcebbc32a413abf166448af24
Dylan Palmboom <nofxboy1234@gmail.com>
2024-07-17 19:48:39 +0200

    Compare with solution

--- src/conditionalRendering/Challenge2.jsx
+++ src/conditionalRendering/Challenge2.jsx
@@ -1,7 +1,10 @@
 function Item({ name, importance }) {
   return (
     <li className="item">
-      {name} {importance > 0 && `(Importance: ${importance})`}
+      {name}
+      {importance > 0 && ' '}
+      {importance > 0 && <i>(Importance: {importance})</i>}
+      {/* {name} {importance > 0 && `(Importance: ${importance})`} */}
     </li>
   );
 }
