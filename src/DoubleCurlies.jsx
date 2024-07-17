c5eadd3b363329b04ba50152da5e8b44283e63e0
Dylan Palmboom <nofxboy1234@gmail.com>
2024-07-17 09:37:49 +0200

    Add DoubleCurlies

---
+++ src/DoubleCurlies.jsx
@@ -0,0 +1,14 @@
+export default function TodoList() {
+  return (
+    <ul
+      style={{
+        backgroundColor: 'grey',
+        color: 'cyan',
+      }}
+    >
+      <li>Improve the videophone</li>
+      <li>Prepare aeronautics lectures</li>
+      <li>Work on the alcohol-fuelled engine</li>
+    </ul>
+  );
+}
