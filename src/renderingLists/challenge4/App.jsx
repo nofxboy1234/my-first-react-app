fda8eeba9f93c4d65fdb20f5b87f4d659be6b9e3
Dylan Palmboom <nofxboy1234@gmail.com>
2024-07-18 10:59:46 +0200

    Add starting files for renderingLists/challenge4

---
+++ src/renderingLists/challenge4/App.jsx
@@ -0,0 +1,17 @@
+const poem = {
+  lines: [
+    'I write, erase, rewrite',
+    'Erase again, and then',
+    'A poppy blooms.',
+  ],
+};
+
+export default function Poem() {
+  return (
+    <article>
+      {poem.lines.map((line, index) => (
+        <p key={index}>{line}</p>
+      ))}
+    </article>
+  );
+}
