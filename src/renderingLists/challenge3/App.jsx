056d741afbf56a7cd85c5e6406904e406b913fc3
Dylan Palmboom <nofxboy1234@gmail.com>
2024-07-18 10:47:46 +0200

    Complete challenge3

--- src/renderingLists/challenge3/App.jsx
+++ src/renderingLists/challenge3/App.jsx
@@ -1,18 +1,29 @@
 import { recipes } from './data.js';
 
+function Recipe({ id, name, ingredients }) {
+  return (
+    <div key={id}>
+      <h2>{name}</h2>
+      <ul>
+        {ingredients.map((ingredient) => (
+          <li key={ingredient}>{ingredient}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
 export default function RecipeList() {
   return (
     <div>
       <h1>Recipes</h1>
       {recipes.map((recipe) => (
-        <div key={recipe.id}>
-          <h2>{recipe.name}</h2>
-          <ul>
-            {recipe.ingredients.map((ingredient) => (
-              <li key={ingredient}>{ingredient}</li>
-            ))}
-          </ul>
-        </div>
+        <Recipe
+          key={recipe.id}
+          id={recipe.id}
+          name={recipe.name}
+          ingredients={recipe.ingredients}
+        />
       ))}
     </div>
   );
