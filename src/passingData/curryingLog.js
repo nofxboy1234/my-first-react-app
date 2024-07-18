3e1f53eed54806747fa8b443f60fe198d11f956d
Dylan Palmboom <nofxboy1234@gmail.com>
2024-07-18 21:18:17 +0200

    Use lodash to curry

--- src/passingData/curryingLog.js
+++ src/passingData/curryingLog.js
@@ -1,5 +1,10 @@
+import _ from 'lodash';
+
 function log(date, importance, message) {
   console.log(
     `[${date.getHours()}:${date.getMinutes()}] [${importance}] ${message}`
   );
 }
+
+log = _.curry(log);
+log(new Date(), 'DEBUG', 'some debug');
