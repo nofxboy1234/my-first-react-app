daac9ccc625e2705819fd786af9b664ec0b99117
Dylan Palmboom <nofxboy1234@gmail.com>
2024-07-18 16:07:02 +0200

    Pass props to Button functional component

--- src/passingData/App.jsx
+++ src/passingData/App.jsx
@@ -1,13 +1,18 @@
-function Button() {
-  return <button>Click Me!</button>;
+function Button(props) {
+  const buttonStyle = {
+    color: props.color,
+    fontSize: props.fontSize + 'px',
+  };
+
+  return <button style={buttonStyle}>{props.text}</button>;
 }
 
 export default function App() {
   return (
     <div>
-      <Button />
-      <Button />
-      <Button />
+      <Button text="Click Me!" color="blue" fontSize={12} />
+      <Button text="Don't Click Me!" color="red" fontSize={12} />
+      <Button text="Click Me!" color="blue" fontSize={20} />
     </div>
   );
 }
