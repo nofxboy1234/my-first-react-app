a0bfbdab6737cadb0f505fb1886c27e3420e31f9
Dylan Palmboom <nofxboy1234@gmail.com>
2024-07-18 10:29:35 +0200

    Complete challenge2

--- src/renderingLists/challenge2/App.jsx
+++ src/renderingLists/challenge2/App.jsx
@@ -4,6 +4,18 @@ export default function RecipeList() {
   return (
     <div>
       <h1>Recipes</h1>
+      {recipes.map((recipe) => {
+        return (
+          <div key={recipe.name}>
+            <h2>{recipe.name}</h2>
+            <ul>
+              {recipe.ingredients.map((ingredient) => (
+                <li key={ingredient}>{ingredient}</li>
+              ))}
+            </ul>
+          </div>
+        );
+      })}
     </div>
   );
 }
