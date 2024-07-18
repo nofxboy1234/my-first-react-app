9f29abc9bfe143c04faff000dc2d3d69387147d0
Dylan Palmboom <nofxboy1234@gmail.com>
2024-07-18 09:16:45 +0200

    Add starting files for renderingLists-Challenge1

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import Hello from './Greeting.jsx';
-import { MacaroniCheese, FriedEggs } from './Food.jsx';
-import MultiElement from './MultiElement.jsx';
+// import App from './App.jsx';
+// import Hello from './Greeting.jsx';
+// import { MacaroniCheese, FriedEggs } from './Food.jsx';
+// import MultiElement from './MultiElement.jsx';
 import './index.css';
-import TodoList from './TodoList.jsx';
-import Bio from './Bio.jsx';
-import Avatar from './Avatar.jsx';
-import ScientistTodoList from './ScientistTodoList.jsx';
-import DateTodoList from './DateTodoList.jsx';
-import Challenge1 from './Challenge1.jsx';
-import Challenge3 from './Challenge3.jsx';
-import AnimalsList from './AnimalsList.jsx';
-import { AnimalApp } from './AnimalComponents.jsx';
-import PackingList from './PackingList.jsx';
-import Challenge1PackingList from './conditionalRendering/Challenge1.jsx';
-import Challenge2PackingList from './conditionalRendering/Challenge2.jsx';
-import Challenge3DrinkList from './conditionalRendering/Challenge3.jsx';
-import PeopleList from './renderingLists/List.jsx';
+// import TodoList from './TodoList.jsx';
+// import Bio from './Bio.jsx';
+// import Avatar from './Avatar.jsx';
+// import ScientistTodoList from './ScientistTodoList.jsx';
+// import DateTodoList from './DateTodoList.jsx';
+// import Challenge1 from './Challenge1.jsx';
+// import Challenge3 from './Challenge3.jsx';
+// import AnimalsList from './AnimalsList.jsx';
+// import { AnimalApp } from './AnimalComponents.jsx';
+// import PackingList from './PackingList.jsx';
+// import Challenge1PackingList from './conditionalRendering/Challenge1.jsx';
+// import Challenge2PackingList from './conditionalRendering/Challenge2.jsx';
+// import Challenge3DrinkList from './conditionalRendering/Challenge3.jsx';
+// import PeopleList from './renderingLists/List.jsx';
+import List from './renderingLists/challenge1/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <PeopleList />
+    <List />
+
+    {/* <PeopleList /> */}
 
     {/* <Challenge3DrinkList /> */}
     {/* <Challenge2PackingList /> */}
