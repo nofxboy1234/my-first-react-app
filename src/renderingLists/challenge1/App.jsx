2b5ae26dd995dd3f8477b58179df033f6ed75e0b
Dylan Palmboom <nofxboy1234@gmail.com>
2024-07-18 09:28:21 +0200

    Complete Challenge1

--- src/renderingLists/challenge1/App.jsx
+++ src/renderingLists/challenge1/App.jsx
@@ -2,20 +2,38 @@ import { people } from './data.js';
 import { getImageUrl } from './utils.js';
 
 export default function List() {
-  const listItems = people.map((person) => (
-    <li key={person.id}>
-      <img src={getImageUrl(person)} alt={person.name} />
-      <p>
-        <b>{person.name}:</b>
-        {' ' + person.profession + ' '}
-        known for {person.accomplishment}
-      </p>
-    </li>
-  ));
+  const chemists = people
+    .filter((person) => person.profession === 'chemist')
+    .map((person) => (
+      <li key={person.id}>
+        <img src={getImageUrl(person)} alt={person.name} />
+        <p>
+          <b>{person.name}:</b>
+          {' ' + person.profession + ' '}
+          known for {person.accomplishment}
+        </p>
+      </li>
+    ));
+
+  const everyoneElse = people
+    .filter((person) => person.profession !== 'chemist')
+    .map((person) => (
+      <li key={person.id}>
+        <img src={getImageUrl(person)} alt={person.name} />
+        <p>
+          <b>{person.name}:</b>
+          {' ' + person.profession + ' '}
+          known for {person.accomplishment}
+        </p>
+      </li>
+    ));
+
   return (
     <article>
-      <h1>Scientists</h1>
-      <ul>{listItems}</ul>
+      <h1>Chemists</h1>
+      <ul>{chemists}</ul>
+      <h1>Everyone Else</h1>
+      <ul>{everyoneElse}</ul>
     </article>
   );
 }
