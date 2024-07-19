0fce52855a9e03bcd249c9a7a8f7585e728bc975
Dylan Palmboom <nofxboy1234@gmail.com>
2024-07-19 12:37:43 +0200

    Set up starting files

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import Gallery from './react-dev/passingProps/challenge1/App';
+
+import App from './odin/introToState/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Gallery />
+    <App />
   </React.StrictMode>
 );
