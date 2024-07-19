c4c62d45e82fa64ecd92ed7768f9450391a2f216
Dylan Palmboom <nofxboy1234@gmail.com>
2024-07-19 16:21:06 +0200

    Add gallery2

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-import Gallery from './react-dev/introToState/gallery/App';
+import Gallery from './react-dev/introToState/gallery2/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
