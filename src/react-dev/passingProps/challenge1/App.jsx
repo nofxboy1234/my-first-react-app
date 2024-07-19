854416b1ad6e7eb84276c1bb93e7ad38a4b11905
Dylan Palmboom <nofxboy1234@gmail.com>
2024-07-19 10:44:43 +0200

    Complete challenge1

--- src/react-dev/passingProps/challenge1/App.jsx
+++ src/react-dev/passingProps/challenge1/App.jsx
@@ -1,57 +1,66 @@
 import { getImageUrl } from './utils.js';
+import { v7 as uuid } from 'uuid';
+
+function Profile({ name, imageID, profession, awards, discovered }) {
+  return (
+    <section className="profile">
+      <h2>{name}</h2>
+      <img
+        className="avatar"
+        src={getImageUrl(imageID)}
+        alt={name}
+        width={70}
+        height={70}
+      />
+      <ul>
+        <li>
+          <b>Profession: </b>
+          {profession}
+        </li>
+        <li>
+          <b>Awards: {awards.length} </b>({awards.join(', ')})
+        </li>
+        <li>
+          <b>Discovered: </b>
+          {discovered}
+        </li>
+      </ul>
+    </section>
+  );
+}
 
 export default function Gallery() {
+  const scientists = [
+    {
+      id: uuid(),
+      name: 'Maria Skłodowska-Curie',
+      imageID: 'szV5sdG',
+      profession: 'physicist and chemist',
+      awards: [
+        'Nobel Prize in Physics',
+        'Nobel Prize in Chemistry',
+        'Davy Medal',
+        'Matteucci Medal',
+      ],
+      discovered: 'polonium (chemical element)',
+    },
+    {
+      id: uuid(),
+      name: 'Katsuko Saruhashi',
+      imageID: 'YfeOqp2',
+      profession: 'geochemist',
+      awards: ['Miyake Prize for geochemistry', 'Tanaka Prize'],
+      discovered: 'a method for measuring carbon dioxide in seawater',
+    },
+  ];
+
   return (
     <div>
       <h1>Notable Scientists</h1>
-      <section className="profile">
-        <h2>Maria Skłodowska-Curie</h2>
-        <img
-          className="avatar"
-          src={getImageUrl('szV5sdG')}
-          alt="Maria Skłodowska-Curie"
-          width={70}
-          height={70}
-        />
-        <ul>
-          <li>
-            <b>Profession: </b>
-            physicist and chemist
-          </li>
-          <li>
-            <b>Awards: 4 </b>
-            (Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal,
-            Matteucci Medal)
-          </li>
-          <li>
-            <b>Discovered: </b>
-            polonium (chemical element)
-          </li>
-        </ul>
-      </section>
-      <section className="profile">
-        <h2>Katsuko Saruhashi</h2>
-        <img
-          className="avatar"
-          src={getImageUrl('YfeOqp2')}
-          alt="Katsuko Saruhashi"
-          width={70}
-          height={70}
-        />
-        <ul>
-          <li>
-            <b>Profession: </b>
-            geochemist
-          </li>
-          <li>
-            <b>Awards: 2 </b>
-            (Miyake Prize for geochemistry, Tanaka Prize)
-          </li>
-          <li>
-            <b>Discovered: </b>a method for measuring carbon dioxide in seawater
-          </li>
-        </ul>
-      </section>
+
+      {scientists.map((scientist) => (
+        <Profile key={scientist.id} {...scientist} />
+      ))}
     </div>
   );
 }
