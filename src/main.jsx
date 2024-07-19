07fe6d05fd561e9f0220889ea350c60574052c2e
Dylan Palmboom <nofxboy1234@gmail.com>
2024-07-19 09:46:45 +0200

    Add starting files

--- src/main.jsx
+++ src/main.jsx
@@ -1,59 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import App from './App.jsx';
-// import Hello from './Greeting.jsx';
-// import { MacaroniCheese, FriedEggs } from './Food.jsx';
-// import MultiElement from './MultiElement.jsx';
 import './index.css';
-// import TodoList from './TodoList.jsx';
-// import Bio from './Bio.jsx';
-// import Avatar from './Avatar.jsx';
-// import ScientistTodoList from './ScientistTodoList.jsx';
-// import DateTodoList from './DateTodoList.jsx';
-// import Challenge1 from './Challenge1.jsx';
-// import Challenge3 from './Challenge3.jsx';
-// import AnimalsList from './AnimalsList.jsx';
-// import { AnimalApp } from './AnimalComponents.jsx';
-// import PackingList from './PackingList.jsx';
-// import Challenge1PackingList from './conditionalRendering/Challenge1.jsx';
-// import Challenge2PackingList from './conditionalRendering/Challenge2.jsx';
-// import Challenge3DrinkList from './conditionalRendering/Challenge3.jsx';
-// import PeopleList from './renderingLists/List.jsx';
-// import List from './renderingLists/challenge1/App';
-// import RecipeList from './renderingLists/challenge3/App';
-// import Poem from './renderingLists/challenge4/App';
-// import TodoList from './odinKeysInReact/TodoList';
-// import MonthList from './odinKeysInReact/MonthList';
-import App from './passingData/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-
-    {/* <MonthList /> */}
-
-    {/* <TodoList /> */}
-    {/* <Poem /> */}
-    {/* <RecipeList /> */}
-    {/* <List /> */}
-    {/* <PeopleList /> */}
-    {/* <Challenge3DrinkList /> */}
-    {/* <Challenge2PackingList /> */}
-    {/* <Challenge1PackingList /> */}
-    {/* <App /> */}
-    {/* <PackingList /> */}
-    {/* <AnimalsList /> */}
-    {/* <AnimalApp /> */}
-    {/* <Hello />
-    <MacaroniCheese />
-    <FriedEggs />
-    <MultiElement />
-    <TodoList />
-    <Bio />
-    <Avatar />
-    <ScientistTodoList />
-    <DateTodoList />
-    <Challenge1 />
-    <Challenge3 /> */}
-  </React.StrictMode>
+  <React.StrictMode></React.StrictMode>
 );
