6e979667b2bfafb53b776ccda2bd242cd845785a
Dylan Palmboom <nofxboy1234@gmail.com>
2024-07-21 11:08:41 +0200

    Add challenge2 starting files

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Gallery from './react-dev/introToState/challenge1/App';
+import Form from './react-dev/introToState/challenge2/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Gallery />
+    <Form />
   </React.StrictMode>
 );
