8fb06e25ccb37ccfe4f42d3588bcf5403bfbbeea
Dylan Palmboom <nofxboy1234@gmail.com>
2024-07-21 11:44:30 +0200

    Add challenge4 starting files

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import FeedbackForm from './react-dev/introToState/challenge3/App';
+import FeedbackForm from './react-dev/introToState/challenge4/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
