a87382fa015c2ee662538ed2085c1cffcdea48a2
Dylan Palmboom <nofxboy1234@gmail.com>
2024-07-23 16:04:32 +0200

    Add Person component for Odin exercise

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './react-dev/sharingState/challenge2/App';
-import './react-dev/sharingState/challenge2/styles.css';
+import App from './odin/moreOnState/person/Person';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
