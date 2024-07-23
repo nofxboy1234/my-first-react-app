b43798c0756709a9a6cefe89b46287df68d602a9
Dylan Palmboom <nofxboy1234@gmail.com>
2024-07-23 14:48:57 +0200

    Add challenge2 starter files

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './react-dev/choosingStateStructure/challenge4/App';
-import './react-dev/choosingStateStructure/challenge4/styles.css';
+import App from './react-dev/sharingState/challenge2/App';
+import './react-dev/sharingState/challenge2/styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
