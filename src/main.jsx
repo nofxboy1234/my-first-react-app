0dbe6bb8b6ac09d102fb27bbf27af86c4ff7c7c9
Dylan Palmboom <nofxboy1234@gmail.com>
2024-07-23 09:31:39 +0200

    Add challenge4 starter files

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './react-dev/choosingStateStructure/challenge3/App';
-import './react-dev/choosingStateStructure/challenge3/styles.css';
+import App from './react-dev/choosingStateStructure/challenge4/App';
+import './react-dev/choosingStateStructure/challenge4/styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
