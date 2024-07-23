97cac05d493c293bdca927020975769286c4c637
Dylan Palmboom <nofxboy1234@gmail.com>
2024-07-23 16:39:41 +0200

    Update Person with first name and last name inputs
    They update full name on change

--- src/odin/moreOnState/person/Person.jsx
+++ src/odin/moreOnState/person/Person.jsx
@@ -1,23 +1,45 @@
 import { useState } from 'react';
 
 export default function Person() {
-  const [person, setPerson] = useState({ name: 'John', age: 100 });
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [age, setAge] = useState(100);
+
+  const fullName = firstName + ' ' + lastName;
 
   const handleIncreaseAge = () => {
-    console.log('in handleIncreaseAge (before setPerson call): ', person);
-    setPerson({ ...person, age: person.age + 1 });
+    console.log('in handleIncreaseAge (before setAge call): ', age);
+    setAge(age + 1);
     // we've called setPerson, surely person has updated?
-    console.log('in handleIncreaseAge (after setPerson call): ', person);
+    console.log('in handleIncreaseAge (after setAge call): ', age);
   };
 
   // this console.log runs every time the component renders
   // what do you think this will print?
-  console.log('during render: ', person);
+  console.log('during render: ', age);
 
   return (
     <>
-      <h1>{person.name}</h1>
-      <h2>{person.age}</h2>
+      <div>
+        <label htmlFor="first-name">First Name </label>
+        <input
+          type="text"
+          id="first-name"
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+      </div>
+
+      <div>
+        <label htmlFor="last-name">Last Name </label>
+        <input
+          type="text"
+          id="last-name"
+          onChange={(e) => setLastName(e.target.value)}
+        />
+      </div>
+
+      <h1>{fullName}</h1>
+      <h2>{age}</h2>
       <button onClick={handleIncreaseAge}>Increase age</button>
     </>
   );
