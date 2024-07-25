59d7ac77090543c487ff0fad7d5c6e4a629e08d6
Dylan Palmboom <nofxboy1234@gmail.com>
2024-07-25 17:04:03 +0200

    Add Clock

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './odin/moreOnState/person/Person';
+import App from './odin/sideEffects/clock/Clock';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
---
+++ src/odin/sideEffects/clock/Clock.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+export default function Clock() {
+  const [counter, setCounter] = useState(0);
+
+  setInterval(() => {
+    setCounter((count) => count + 1);
+  }, 1000);
+
+  return <p>{counter} seconds have passed.</p>;
+}
