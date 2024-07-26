7b78c9f3afc1ba5b6442a474e6db75bdb8345a98
Dylan Palmboom <nofxboy1234@gmail.com>
2024-07-26 12:08:34 +0200

    Add Chat Room

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './odin/sideEffects/addition/AdditionDisplay';
+import App from './react-dev/sideEffects/chatRoom/App';
+import './react-dev/sideEffects/chatRoom/styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
