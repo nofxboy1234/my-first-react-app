aabf4a368896e14a7f7be04c2e172d3d09eec0c9
Dylan Palmboom <nofxboy1234@gmail.com>
2024-07-26 15:57:54 +0200

    Update challenge3 solution

--- src/react-dev/sideEffects/challenge3/App.jsx
+++ src/react-dev/sideEffects/challenge3/App.jsx
@@ -4,16 +4,15 @@ export default function App() {
   const [position, setPosition] = useState({ x: 0, y: 0 });
   const [canMove, setCanMove] = useState(true);
 
-  function handleMove(e) {
-    if (canMove) {
-      setPosition({ x: e.clientX, y: e.clientY });
-    }
-  }
-
   useEffect(() => {
+    function handleMove(e) {
+      if (canMove) {
+        setPosition({ x: e.clientX, y: e.clientY });
+      }
+    }
+
     window.addEventListener('pointermove', handleMove);
     return () => window.removeEventListener('pointermove', handleMove);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [canMove]);
 
   return (
