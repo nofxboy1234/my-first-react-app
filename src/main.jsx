922265f9dd729f76f84287c52d6efbb2dd1266a3
Dylan Palmboom <nofxboy1234@gmail.com>
2024-07-26 14:46:36 +0200

    Add challenge2 starter files

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './react-dev/sideEffects/challenge1/App';
+import App from './react-dev/sideEffects/challenge2/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
