5731505acdf643a6956e84be30c9e14402119a18
Dylan Palmboom <nofxboy1234@gmail.com>
2024-07-26 14:36:16 +0200

    Add challenge1 starter files

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './react-dev/sideEffects/chatRoom/App';
-import './react-dev/sideEffects/chatRoom/styles.css';
+import App from './react-dev/sideEffects/challenge1/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
