a139bde2864e8793e3a72a07f45dc6e433f1aa38
Dylan Palmboom <nofxboy1234@gmail.com>
2024-07-26 15:48:27 +0200

    Comment out disabling of rules in eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,7 +17,7 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
-    'react/prop-types': 'off',
-    'react/no-unescaped-entities': 'off',
+    // 'react/prop-types': 'off',
+    // 'react/no-unescaped-entities': 'off',
   },
 };
