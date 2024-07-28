979c4912acb173fa8acf395f8d8613345a4ecbf1
Dylan Palmboom <nofxboy1234@gmail.com>
2024-07-28 07:36:54 +0200

    Complete challenge4

--- src/react-dev/sideEffects/challenge4/ChatRoom.jsx
+++ src/react-dev/sideEffects/challenge4/ChatRoom.jsx
@@ -5,8 +5,7 @@ export default function ChatRoom({ roomId, createConnection }) {
     const connection = createConnection(roomId);
     connection.connect();
     return () => connection.disconnect();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [roomId]);
+  }, [roomId, createConnection]);
 
   return <h1>Welcome to the {roomId} room!</h1>;
 }
