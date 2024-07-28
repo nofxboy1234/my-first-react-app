19e2a0e09aeb91fd02ac9892340e5eaa309c338b
Dylan Palmboom <nofxboy1234@gmail.com>
2024-07-28 07:22:01 +0200

    Add challenge4 starter files

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './react-dev/sideEffects/challenge3/App';
+import App from './react-dev/sideEffects/challenge4/App';
+import './react-dev/sideEffects/challenge4/styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
