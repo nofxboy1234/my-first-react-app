533682a2088b039ddfcd8f1aa0e8647130af17e3
Dylan Palmboom <nofxboy1234@gmail.com>
2024-07-30 09:07:08 +0200

    Update starter files with starting code

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './react-dev/sideEffects/challenge5/App';
-import './react-dev/sideEffects/challenge5/styles.css';
+import App from './react-dev/sideEffects2/challenge1/App';
+import './react-dev/sideEffects2/challenge1/styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
