fc37192509828ad990ec706d308752d1d515a55a
Dylan Palmboom <nofxboy1234@gmail.com>
2024-07-30 14:54:23 +0200

    Disable strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client';
 import App from './dmitripavlutin/CountInputChanges';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App />
+  // <React.StrictMode>
+  // </React.StrictMode>
 );
