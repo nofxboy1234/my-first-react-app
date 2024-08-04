5e881361f9c1b5e06299d710b41d4e0e9bb3a264
Dylan Palmboom <nofxboy1234@gmail.com>
2024-08-04 23:03:33 +0200

    Add test that renders a React Component

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-// import App from './odin/classBasedComponents/FunctionalInput';
-import App from './odin/classBasedComponents/ClassInput';
+import App from './odin/classBasedComponents/FunctionalInput';
+// import App from './odin/classBasedComponents/ClassInput';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
