1ce92f4bae81a2b522aa372eb5f1bfe4fefbdf13
Dylan Palmboom <nofxboy1234@gmail.com>
2024-08-04 23:54:05 +0200

    Write first proper test

--- tests/App.test.jsx
+++ tests/App.test.jsx
@@ -1,16 +1,26 @@
+import { describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
+import App from '../src/odin/introToReact/App';
 
-import App from '../src/odin/classBasedComponents/FunctionalInput';
-
-describe('App', () => {
-  it('renders headline', () => {
-    render(<App title="React" />);
-
-    screen.debug();
-
-    // check if App components renders headline
+describe('App component', () => {
+  it('renders correct heading', () => {
+    render(<App />);
+    expect(screen.getByRole('heading').textContent).toMatch(/our first test/i);
   });
 });
+
+// import { render, screen } from '@testing-library/react';
+// import App from '../src/odin/classBasedComponents/FunctionalInput';
+
+// describe('App', () => {
+//   it('renders headline', () => {
+//     render(<App title="React" />);
+
+//     screen.debug();
+
+//     // check if App components renders headline
+//   });
+// });
 
 // describe('something truthy and falsy', () => {
 //   it('true to be true', () => {
