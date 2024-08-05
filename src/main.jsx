8a7b909690e1624db38252ee8d51d4e161fe0759
Dylan Palmboom <nofxboy1234@gmail.com>
2024-08-05 09:39:57 +0200

    Add HeadingChanger

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './odin/classBasedComponents/FunctionalInput';
+// import App from './odin/classBasedComponents/FunctionalInput';
 // import App from './odin/classBasedComponents/ClassInput';
+import App from './odin/introToReactTesting/HeadingChanger';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
