70505ea8eec234aa097d30e5a032a1cd69dd4c75
Dylan Palmboom <nofxboy1234@gmail.com>
2024-08-06 13:32:52 +0200

    Use isRequired in propTypes and add defaultProps

--- src/odin/propTypes/renderName/RenderName.jsx
+++ src/odin/propTypes/renderName/RenderName.jsx
@@ -1,12 +1,16 @@
-import PropTypes from 'prop-types';
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const RenderName = (props) => {
   return <div>{props.name}</div>;
 };
 
 RenderName.propTypes = {
-  name: PropTypes.string,
+  name: PropTypes.string.isRequired,
+};
+
+RenderName.defaultProps = {
+  name: 'Zach',
 };
 
 export default RenderName;
