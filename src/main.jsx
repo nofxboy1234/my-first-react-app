130a88639e72c809a1a75f4219bbcedad8544293
Dylan Palmboom <nofxboy1234@gmail.com>
2024-08-06 18:25:32 +0200

    Add Profile

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './odin/propTypes/renderName/RenderName';
+import App from './odin/reactRouter/profile/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
---
+++ src/odin/reactRouter/profile/App.jsx
@@ -0,0 +1,17 @@
+const App = () => {
+  return (
+    <div>
+      <h1>Hello from the main page of the app!</h1>
+      <p>Here are some examples of links to other pages</p>
+      <nav>
+        <ul>
+          <li>
+            <a href="profile">Profile page</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default App;
