5a6df768f7c97b07871f049437eed277e9be36fc
Dylan Palmboom <nofxboy1234@gmail.com>
2024-08-06 22:54:24 +0200

    Add errorElement to router config

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './odin/reactRouter/profile/App';
 import Profile from './odin/reactRouter/profile/Profile';
+import ErrorPage from './odin/reactRouter/profile/ErrorPage';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+    errorElement: <ErrorPage />,
   },
   {
     path: 'profile/:name',
