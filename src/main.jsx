24df0d81cb8e2da621f269f1e8367dd3d58409ed
Dylan Palmboom <nofxboy1234@gmail.com>
2024-08-06 13:17:19 +0200

    Add RenderName to layout

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './odin/introToReactTesting/HeadingChanger';
+import App from './odin/propTypes/renderName/RenderName';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
