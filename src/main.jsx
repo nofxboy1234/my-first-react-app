129193a9a13e6894dd24749d37f7cbed2718d90a
Dylan Palmboom <nofxboy1234@gmail.com>
2024-08-06 18:52:12 +0200

    Install react-router-dom and update main

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './odin/reactRouter/profile/App';
+import Profile from './odin/reactRouter/profile/Profile';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: 'profile',
+    element: <Profile />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
