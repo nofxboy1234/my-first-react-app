a4e39c1c896b35cc53e0bb5728cba3bb534e68a5
Dylan Palmboom <nofxboy1234@gmail.com>
2024-08-07 11:41:59 +0200

    Use Navigate component to show root page after registering

--- src/odin/reactRouter/profile/Register.jsx
+++ src/odin/reactRouter/profile/Register.jsx
@@ -1,11 +1,18 @@
+import { useState } from 'react';
+import { Navigate } from 'react-router-dom';
+
 const Register = () => {
+  const [registered, setRegistered] = useState(false);
+
   function register(e) {
     e.preventDefault();
     console.log('registered!');
+    setRegistered(true);
   }
 
   return (
     <>
+      {registered && <Navigate to="/" replace={true} />}
       <form onSubmit={register}>
         <input type="text" />
         <button type="submit">Register</button>
