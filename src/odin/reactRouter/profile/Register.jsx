c01514d213cf6ea6b40a5c9859914aeedf3e7da6
Dylan Palmboom <nofxboy1234@gmail.com>
2024-08-07 11:35:52 +0200

    Show form on Register

--- src/odin/reactRouter/profile/Register.jsx
+++ src/odin/reactRouter/profile/Register.jsx
@@ -1,5 +1,17 @@
 const Register = () => {
-  return <div>Register!</div>;
+  function register(e) {
+    e.preventDefault();
+    console.log('registered!');
+  }
+
+  return (
+    <>
+      <form onSubmit={register}>
+        <input type="text" />
+        <button type="submit">Register</button>
+      </form>
+    </>
+  );
 };
 
 export default Register;
