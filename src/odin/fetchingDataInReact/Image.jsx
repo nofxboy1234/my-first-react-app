8885cf7d4ddd9ee7cd82958b474b26ce55bdf27c
Dylan Palmboom <nofxboy1234@gmail.com>
2024-08-07 15:20:31 +0200

    Add error handling

--- src/odin/fetchingDataInReact/Image.jsx
+++ src/odin/fetchingDataInReact/Image.jsx
@@ -2,18 +2,26 @@ import { useEffect, useState } from 'react';
 
 const Image = () => {
   const [imageURL, setImageURL] = useState(null);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     console.log('fetch');
     fetch('https://jsonplaceholder.typicode.com/photos', {
       mode: 'cors',
     })
-      .then((response) => response.json())
+      .then((response) => {
+        if (response.status >= 400) {
+          throw new Error('server error');
+        }
+        return response.json();
+      })
       .then((data) => setImageURL(data[0].url))
-      .catch((error) => console.error(error));
+      .catch((error) => setError(error));
   }, []);
 
   console.log('render');
+
+  if (error) return <p>A network error was encountered</p>;
 
   return (
     imageURL && (
