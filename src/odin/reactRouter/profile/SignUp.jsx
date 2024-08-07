624d7f2933ca965a75e7bf0d56c1675636fcfdad
Dylan Palmboom <nofxboy1234@gmail.com>
2024-08-07 11:32:15 +0200

    Update signup message

--- src/odin/reactRouter/profile/SignUp.jsx
+++ src/odin/reactRouter/profile/SignUp.jsx
@@ -1,9 +1,12 @@
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 const SignUp = () => {
+  const { name } = useParams();
+
   return (
     <>
       <div>
+        {name && `${name[0].toUpperCase()}${name.slice(1)}, `}
         <Link to="/register">Sign up</Link> now if you'd like your own Profile
         page!
       </div>
