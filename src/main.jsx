9f5cfc3fc97ea5d07ce6e9e2b2596f9870785e73
Dylan Palmboom <nofxboy1234@gmail.com>
2024-08-09 12:35:21 +0200

    Give component a style as a prop

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './odin/cssModules/Example';
+// import App from './odin/cssModules/Example';
+
+import App from './odin/cssModules/Cat';
+import themeA from './odin/cssModules/cat.module.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+  <App theme={themeA} />
   // <React.StrictMode>
   // </React.StrictMode>
 );
---
+++ src/odin/cssModules/Cat.jsx
@@ -0,0 +1,12 @@
+// import styles from './cat.module.css';
+
+const Cat = ({ theme }) => {
+  return (
+    <div className={theme.component}>
+      <div>hello</div>
+      <p>bye</p>
+    </div>
+  );
+};
+
+export default Cat;
