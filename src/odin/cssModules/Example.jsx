616381025c1566281795c4db672b1dc47e793205
Dylan Palmboom <nofxboy1234@gmail.com>
2024-08-09 10:33:05 +0200

    Compose css selectors together

--- src/odin/cssModules/Example.jsx
+++ src/odin/cssModules/Example.jsx
@@ -9,6 +9,12 @@ const Example = () => {
       </div>
       <div className={dylan2}>Example using named css-modules imports</div>
       <div className="hello-bye">Example using standard css import</div>
+      <div className={classes.className}>
+        Example using composed css selectors
+      </div>
+      <div className={classes.otherClassName}>
+        Example using composed css selectors
+      </div>
     </>
   );
 };
