b0cdd96dc6d7b926f581bcd86b3c9c9bd5961072
Dylan Palmboom <nofxboy1234@gmail.com>
2024-08-09 10:08:32 +0200

    Test out Css Modules in Vite

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './odin/fetchingDataInReact/Image';
+
+import App from './odin/cssModules/Example';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <App />
---
+++ src/odin/cssModules/Example.jsx
@@ -0,0 +1,7 @@
+import classes from './example.module.css';
+
+const Example = () => {
+  return <div className={classes.dylan}>Example</div>;
+};
+
+export default Example;
