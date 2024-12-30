c83c279b95282ec79dc66268175ded7a3d38aad8
Dylan Palmboom <nofxboy1234@gmail.com>
2024-12-30 09:02:34 +0200

    Test setting state to the same integer value

--- src/odin/cssModules/Demo/Demo.jsx
+++ src/odin/cssModules/Demo/Demo.jsx
@@ -1,13 +1,23 @@
 let styles = {};
 import demo from './Demo.module.css';
 import fancyButton from './css-fancy-button.module.css';
+import { useState } from 'react';
+
 Object.assign(styles, fancyButton, demo);
 
 const Demo = () => {
+  const [count, setCount] = useState(0);
+
   return (
     <>
       <div className={styles.demo}>Demo</div>
       <div className={styles.fancyButton}>Demo</div>
+      <br />
+      <div>Count: {count}</div>
+      <button onClick={() => setCount(count + 1)}>Increment Count</button>
+      <button onClick={() => setCount(count)}>
+        Set Count to the same value
+      </button>
     </>
   );
 };
